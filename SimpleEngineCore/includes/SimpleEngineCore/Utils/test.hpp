482c22fda67ecbd0c0b1bf6a6dd50476497a24da
Anatoly <kp_anat@mail.ru>
2024-01-10 21:13:05 +0300

    Added CMakeLists.txt with two targets

---
+++ SimpleEngineCore/includes/SimpleEngineCore/Utils/test.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace SimpleEngine {
+	void sayHello();
+}
---
+++ SimpleEngineCore/src/SimpleEngineCore/Utils/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <SimpleEngineCore/Utils/test.hpp>
+namespace SimpleEngine {
+	void sayHello() {
+		std::cout << "Hello from Simple Engine Core" << std::endl;
+	}
+}
